27d5758661c4a1de5fa92c5eac6dc0a2eb64c635
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-06-10 18:25:32 -0700

    first commit

---
+++ Section 2/part 1/first.py
@@ -0,0 +1,16 @@
+"""
+the avg function takes a list of numbers, and returns the their average.
+@:return the average.
+@:arg list of numbers.
+
+Ex. print(avg(1, 2, 3, 43))
+"""
+
+
+def avg(*list_num):
+	total = 0
+	for num in list_num:
+		total += num
+	return total / len(list_num)
+
+
---
+++ Section 2/part 1/first_challenge.py
@@ -0,0 +1,22 @@
+"""This class is going to test the avg function.
+"""
+import unittest
+
+
+class TestAvg(unittest.TestCase):
+
+    def test_easy(self):
+        print(first.avg(10, 10, 10, 10, 10))
+
+    def test_medium(self):
+        pass
+
+    def test_hard(self):
+        pass
+
+
+if __name__ == '__main__':
+    import first
+    unittest.main()
+
+
